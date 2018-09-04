36aa08fb414e1e6619787c55d425af3ca303a403
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-09-04 12:26:21 -0400

    Delete Directory C#
    Delete Directory C#
    On this tutorial, you will learn how to Fully deleted a directory and its content using C#.

---
+++ DeleteDirectory/DeleteDirectory/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DeleteDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtDirectoryPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(414, 75);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Delete Directory";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtDirectoryPath
+            // 
+            this.txtDirectoryPath.Location = new System.Drawing.Point(13, 94);
+            this.txtDirectoryPath.Name = "txtDirectoryPath";
+            this.txtDirectoryPath.Size = new System.Drawing.Size(413, 20);
+            this.txtDirectoryPath.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 122);
+            this.Controls.Add(this.txtDirectoryPath);
+            this.Controls.Add(this.btnDelete);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtDirectoryPath;
+    }
+}
+
---
+++ DeleteDirectory/DeleteDirectory/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteDirectory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Delete_Full_Directory(String dir)
+        {
+
+            try
+            {
+                string[] files = Directory.GetFiles(dir);
+                string[] dirs = Directory.GetDirectories(dir);
+
+                foreach (string file in files)
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    File.Delete(file);
+                }
+
+                foreach (string dir_2 in dirs)
+                {
+                    Delete_Full_Directory(dir_2);
+                }
+
+                Directory.Delete(dir, false);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtDirectoryPath.Text.Length == 0)
+            {
+                MessageBox.Show("Please Enter Path to be Deleted!");
+                return;
+            }
+
+            if (Directory.Exists(txtDirectoryPath.Text))
+            {
+                Delete_Full_Directory(txtDirectoryPath.Text);
+            }
+            else
+            {
+                MessageBox.Show("Directory Does Not Exist!!");
+                return;
+            }
+
+
+            //re-confirmation
+            if (!File.Exists(txtDirectoryPath.Text))
+            {
+                MessageBox.Show("Directory Fully Deleted!");
+            }
+
+        }
+    }
+}
